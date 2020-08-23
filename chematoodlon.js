d965b7cfda599bda1ed2eccd7b1d34752401fd31
shubhro <bshubhra@gmail.com>
2020-08-24 01:16:01 +0530

    Update chematoodlon.js

--- chematoodlon.js
+++ chematoodlon.js
@@ -21,3 +21,39 @@ function one(){
 		},3000);  
 });*/
 
+//Showing and hiding the top menu on clicking the menu button & clicking outside respectively:	
+	var mb = document.getElementById("menuButtonM");
+	var mi = document.getElementById("mtItemsM");
+	var mc = document.getElementById("menuCloseButtonM");
+						
+			var n=0;
+			
+			window.addEventListener('click',function(e){
+					
+				if(mb.contains(e.target)){n=1;}
+				else{n=0;}
+				
+				if(mc.contains(e.target)){
+				mb.style="initial";
+				mi.style="initial";			
+				}
+				
+				if(n==0){
+					if(mi.contains(e.target)){n==1}
+					else{
+					mb.style="initial";
+					mi.style="initial";
+					}
+				}
+				
+				if(n==1){
+				mb.style.visibility = "hidden";
+				mb.style.opacity="0";			
+				mb.addEventListener('transitionend',function(){
+				n=1;
+				
+				mb.style.display = "none";
+				mi.style.display = "flex";
+				mi.style.visibility = "visible";})			
+				}			
+			})
