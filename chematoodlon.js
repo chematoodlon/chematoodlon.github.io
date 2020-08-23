dd857b439eb9a00647bc41b09fc1eb70a2c76ee3
shubhro <bshubhra@gmail.com>
2020-08-23 22:04:35 +0530

    Update chematoodlon.js

--- chematoodlon.js
+++ chematoodlon.js
@@ -1,25 +1,34 @@
-/*var by = document.getElementsByTagName('body')[0];
+var by = document.getElementsByTagName('body')[0];
 var lr = document.getElementById("loader");
 
 var n,lt;
 window.onload = function(){	
 	lt = new Date().getTime() - window.performance.timing.navigationStart;
-	if(lt>=8000){n=1;}
-	else if(lt<8000){n=0;}
+	if(lt>=5000){n=1;}
+	else if(lt<5000){n=0;}
 	one();
-}*/
-/*function one(){
+}
+function one(){
 	if(n==0){
 		setTimeout(function(){
 			console.log("st");
-			lr.style.display="none";},8000);
+			lr.style.opacity="0";
+			setTimeout(function(){
+				lr.style.display="none";
+			},2000);
+			},5000);
 	}	
 	if(n==1){
 		setTimeout(function(){
 			console.log("st2");
-			lr.style.display="none";},lt);
+			lr.style.opacity="0";
+			setTimeout(function(){
+				lr.style.display="none";
+			},2000);
+			},lt);
 	}
 }
+
 
 /*$(document).ready(function() {
 	setTimeout(function(){
