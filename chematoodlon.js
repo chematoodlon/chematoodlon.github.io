492b47a0eddcd2f8663f6d8b45e8b324ccb82ccd
shubhro <bshubhra@gmail.com>
2020-08-26 01:01:26 +0530

    Update chematoodlon.js

--- chematoodlon.js
+++ chematoodlon.js
@@ -57,3 +57,73 @@ function one(){
 				mi.style.visibility = "visible";})			
 				}			
 			})
+
+//Volume on off:
+var vol = document.getElementById("Volume");
+var spk1 = document.getElementById("Speaker1");
+var spk2 = document.getElementById("Speaker2");
+var snd = document.getElementById("Sound");
+var sno = document.getElementById("No_sound");
+
+var bg1 = document.getElementById("bgAudio1");
+var bg2 = document.getElementById("bgAudio2");
+
+var m=0;
+
+window.addEventListener('click',function(g){
+	if(m>=1||vol.contains(g.target)){
+		if(spk1.contains(g.target)||snd.contains(g.target)){
+			snd.style.display="none";
+			spk1.style.display="none";
+			sno.style.display="block";
+			spk2.style.display="block";
+			bg1.pause();
+			bg2.pause();
+		}
+		if(spk2.contains(g.target)||sno.contains(g.target)){
+			snd.style.display="initial";
+			spk1.style.display="initial";
+			sno.style.display="none";
+			spk2.style.display="none";
+			if(bg1.muted=true){bg2.play();}
+			else{
+				bg1.play();
+				bg1.addEventListener('ended',function(){
+					bg1.muted=true;
+					bg2.play();
+				});	
+			}
+		}
+		m++;
+		}
+	else if(m==0){	
+		snd.style.display="initial";
+		spk1.style.display="initial";
+		sno.style.display="none";
+		spk2.style.display="none";
+		bg1.play();
+		bg1.addEventListener('ended',function(){
+			bg1.muted=true;
+			bg2.play();
+		});			
+	}
+})
+	
+/*else if(m>0){
+	window.addEventListener('click',function(f){
+		if(spk1.contains(f.target)||snd.contains(f.target)){
+			snd.style.display="none";
+			spk1.style.display="none";
+			sno.style.display="block";
+			spk2.style.display="block";
+		}
+		if(spk2.contains(f.target)||sno.contains(f.target)){
+			snd.style.display="initial";
+			spk1.style.display="initial";
+			sno.style.display="none";
+			spk2.style.display="none";
+		}		
+	})
+}*/
+
+
